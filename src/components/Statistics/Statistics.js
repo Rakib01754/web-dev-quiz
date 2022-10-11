62f834cdb6e6185b7f52c204154fde33966d6c7e
Rakib01754 <rakibul.freelancer567@gmail.com>
2022-10-11 13:52:50 +0600

    statistics page updated

--- src/components/Statistics/Statistics.js
+++ src/components/Statistics/Statistics.js
@@ -1,10 +1,25 @@
 import React from 'react';
+import { useLoaderData } from 'react-router-dom';
+import { BarChart, Bar, XAxis, YAxis, Tooltip, Legend, CartesianGrid, ResponsiveContainer } from 'recharts';
 
 const Statistics = () => {
+    const data = useLoaderData()
     return (
+
         <div>
-            <h1>Statistics</h1>
+            <h1 className='font-bold text-3xl underline my-5'>Number Of Quizzes</h1>
+            <ResponsiveContainer width={'70%'} height={300} className='mx-auto'>
+                <BarChart width={600} height={300} data={data.data}>
+                    <XAxis dataKey="name" stroke="#8884d8" />
+                    <YAxis />
+                    <Tooltip wrapperStyle={{ width: 100, backgroundColor: '#ccc' }} />
+                    <Legend width={100} wrapperStyle={{ top: 40, right: 20, backgroundColor: '#f5f5f5', border: '1px solid #d5d5d5', borderRadius: 3, lineHeight: '40px' }} />
+                    <CartesianGrid stroke="#ccc" strokeDasharray="5 5" />
+                    <Bar dataKey="total" fill="#8884d8" barSize={30} />
+                </BarChart>
+            </ResponsiveContainer>
         </div>
+
     );
 };
 
