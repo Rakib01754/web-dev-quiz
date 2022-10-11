5cd1b3de790117d4f92a1b5cdf3807a64cd99651
Rakib01754 <rakibul.freelancer567@gmail.com>
2022-10-11 14:12:48 +0600

    footer added

--- src/components/Main/Main.js
+++ src/components/Main/Main.js
@@ -1,12 +1,14 @@
 import React from 'react';
 import { Outlet } from 'react-router-dom';
+import Footer from '../Footer/Footer';
 import Header from '../Header/Header';
 
 const Main = () => {
     return (
-        <div>
+        <div className='max-w-[1440px] mx-auto'>
             <Header></Header>
             <Outlet></Outlet>
+            <Footer></Footer>
         </div>
     );
 };
